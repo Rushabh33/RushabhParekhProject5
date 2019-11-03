7dba24513fbdb86f1bbd1df82d4dec1b3f05d2bd
Rushabh Parekh <rushabh.parekh.11@gmail.com>
2019-11-03 13:13:41 -0500

    Updates

---
+++ precache-manifest.450ff22ae6ef667724429a5dd15feca0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e410d6422d023b00d95d5a67e95cafae",
+    "url": "/theBeerStoreAPI-reactJS/index.html"
+  },
+  {
+    "revision": "d9a58ac5ab671030b782",
+    "url": "/theBeerStoreAPI-reactJS/static/css/main.951248c4.chunk.css"
+  },
+  {
+    "revision": "285bb1765ca482435df5",
+    "url": "/theBeerStoreAPI-reactJS/static/js/2.55d320c6.chunk.js"
+  },
+  {
+    "revision": "d9a58ac5ab671030b782",
+    "url": "/theBeerStoreAPI-reactJS/static/js/main.b5710e03.chunk.js"
+  },
+  {
+    "revision": "c16bd7ee5ccb99827bf7",
+    "url": "/theBeerStoreAPI-reactJS/static/js/runtime-main.2a773944.js"
+  },
+  {
+    "revision": "4e52120f348b78783032b82c6f950d36",
+    "url": "/theBeerStoreAPI-reactJS/static/media/bannerBackground.4e52120f.jpg"
+  },
+  {
+    "revision": "1ed01802a27f3a0b3c08877bc12ffa64",
+    "url": "/theBeerStoreAPI-reactJS/static/media/filterIcon.1ed01802.svg"
+  },
+  {
+    "revision": "1182456c2bca0a7aaaf1bdc1f3ca7e73",
+    "url": "/theBeerStoreAPI-reactJS/static/media/placeHolderImage.1182456c.svg"
+  },
+  {
+    "revision": "2ea4c43614e5b0beff1d450e9d280ea2",
+    "url": "/theBeerStoreAPI-reactJS/static/media/searchIcon.2ea4c436.svg"
+  },
+  {
+    "revision": "b011b91835b22878af8087794d496a87",
+    "url": "/theBeerStoreAPI-reactJS/static/media/theBeerStoreLogo.b011b918.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de05a16d39ef10a7032d4b3b4bcfff12.js"
+  "/theBeerStoreAPI-reactJS/precache-manifest.450ff22ae6ef667724429a5dd15feca0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/theBeerStoreAPI-reactJS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
